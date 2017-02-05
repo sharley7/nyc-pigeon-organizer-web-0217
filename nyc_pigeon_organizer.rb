1c322d40a7fd5b9f739bb8a2502ae4d431c2ba1d
S. Marie Kav <siobhan.harley@gmail.com>
2017-02-05 05:15:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+name_hash = {}
+  data.collect do |bird, category|
+   category.collect do | subcategory, data|
+      data.collect do | item |
+          name_hash[item] ||= {}
+          name_hash[item][bird] ||= []
+          name_hash[item][bird] << subcategory.to_s
+          end
+        end
+      end
+    name_hash
+  end
